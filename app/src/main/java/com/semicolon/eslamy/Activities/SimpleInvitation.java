392cc2e11579b837ffba4079e84a01b309c9a545
elashry <mohamedelashry323@gmail.com>
2018-03-25 22:19:40 +0200

    init

--- app/src/main/java/com/semicolon/eslamy/Activities/SimpleInvitation.java
+++ app/src/main/java/com/semicolon/eslamy/Activities/SimpleInvitation.java
@@ -11,5 +11,7 @@ public class SimpleInvitation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_invitation);
+
+
     }
 }
