3d520650f8e33f082600d5d5bdb246a00ce3798b
elashry <mohamedelashry323@gmail.com>
2018-03-27 00:02:53 +0200

    init

--- app/src/main/java/com/semicolon/eslamy/Activities/WebViewActivity.java
+++ app/src/main/java/com/semicolon/eslamy/Activities/WebViewActivity.java
@@ -3,7 +3,9 @@ package com.semicolon.eslamy.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.semicolon.eslamy.R;
@@ -20,9 +22,19 @@ public class WebViewActivity extends AppCompatActivity {
         getDataFromIntent();
         webView =  findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.setWebViewClient(new HelloWebViewClient());
         webView.loadUrl(url);
     }
-
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+    }
     private void getDataFromIntent() {
 
         Intent intent = getIntent();
